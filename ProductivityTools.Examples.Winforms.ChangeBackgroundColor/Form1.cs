0b678350c2384b918c533ae9fa7b4b24f8966f43
pwujczyk <pwujczyk@ecovadis.com>
2018-08-16 22:13:44 +0200

    iinitial commit

---
+++ ProductivityTools.Examples.Winforms.ChangeBackgroundColor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductivityTools.Examples.Winforms.ChangeBackgroundColor
+{
+    delegate void ChangeColorDelegate(Color c);
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        Boolean b;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            thread = new Thread(ChangeColorBackground);
+            thread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b = false;
+        }
+
+        private void ChangeColorBackground()
+        {
+            while (true)
+            {
+                if (b == true)
+                {
+                    Random rnd = new Random();
+                    int r = rnd.Next(0, 254);
+                    int n = rnd.Next(0, 254);
+                    int d = rnd.Next(0, 254);
+                    Color c = Color.FromArgb(r, n, d);
+                    ChangeColorThreadSafe(c);
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void ChangeColorThreadSafe(Color c)
+        {
+            if (button1.InvokeRequired)
+            {
+                ChangeColorDelegate delgate = new ChangeColorDelegate(ChangeColorThreadSafe);
+                button1.Invoke(delgate, new object[] { c });
+            }
+            else
+            {
+                this.BackColor = c;
+                //   Application.DoEvents();
+            }
+        }
+    }
+}
